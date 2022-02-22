64bbc0f1359c539fc65fc2dd41e5df9de82452fb
MadRoosterCro <dario.car.kz@gmail.com>
2022-02-22 21:59:58 +0100

    Setting up ReduxStore

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import ReactDOM from 'react-dom';
-import EventComponent from './events/EventComponent';
-import UserSearch from './refs/UserSearch';
 
 const App = () => {
   return (
     <div>
-      <UserSearch />
+      <h3>Hi there!</h3>
     </div>
   );
 };
