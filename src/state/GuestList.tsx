3d3f77e45994ba3612000d43170f8a285cd9b49d
MadRoosterCro <dario.car.kz@gmail.com>
2022-02-22 18:18:19 +0100

    Created guestList component

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 
 const GuestList: React.FC = () => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState<string>('');
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const guestHandler = () => {
+    setName('');
+    setGuests([...guests, name]);
+  };
+
   return (
     <div>
       <h3>Guest List</h3>
+      <ul>
+        {guests.map((guest) => (
+          <li key={guest}>{guest}</li>
+        ))}
+      </ul>
 
       <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button>Add Guest</button>
+      <button onClick={guestHandler}>Add Guest</button>
     </div>
   );
 };
